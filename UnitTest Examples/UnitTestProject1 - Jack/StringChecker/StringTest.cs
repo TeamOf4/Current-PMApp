9006b854bbb436044a4477c07c75c3392e053f05
Jack Sanger <jack.sanger@student.anglia.ac.uk>
2015-10-29 09:51:28 +0000

    Added my test plans from last weeks lesson

---
+++ UnitTest Examples/UnitTestProject1 - Jack/StringChecker/StringTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringChecker
+{
+    public class StringTest
+    {
+        public bool CheckString(string input, string input2)
+        {
+            if (input == "" || input2 == "")
+            {
+                return false;
+            }
+
+            else if (input == input2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }          
+        }
+    }
+}
---
+++ UnitTest Examples/UnitTestProject1 - Jack/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ShouldBeTheSameString()
+        {
+            //ARRANGE
+
+            var checker = new StringChecker.StringTest();
+            
+            //ACT
+
+            var result = checker.CheckString("test", "test");
+
+            //ASSERT
+
+            Assert.AreEqual(true, result);
+        }
+        [TestMethod]
+        public void testStringAredifferent()
+        {
+            //ARRANGE
+
+            var checker = new StringChecker.StringTest();
+
+            //ACT
+
+            var result = checker.CheckString("test1", "test2");
+
+            //ASSERT
+
+            Assert.AreNotEqual(true ,result);
+        }
+        [TestMethod]
+        public void testStringAredifferent()
+        {
+            //ARRANGE
+
+            var checker = new StringChecker.StringTest();
+
+            //ACT
+
+            var result = checker.CheckString("test1", "test2");
+
+            //ASSERT
+
+            Assert.AreNotEqual(true, result);
+        }
+
+
+         
+
+
+
+    
+    }
+}
